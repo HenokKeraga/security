a409d1e1adeecec7b757692d001ffa2cb9f56c7a
henok keraga <henokkeraga@gmail.com>
2023-04-08 06:01:08 -0600

    first

---
+++ sp-se-m1/sp-se-m1/src/main/java/edu/miu/spsem1/SpSeM1Application.java
@@ -0,0 +1,13 @@
+package edu.miu.spsem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSeM1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpSeM1Application.class, args);
+    }
+
+}
---
+++ sp-se-m1/sp-se-m1/src/test/java/edu/miu/spsem1/SpSeM1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.spsem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpSeM1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
